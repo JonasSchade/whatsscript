7127c4025a4ac71bdaa92917539be934e2331091
Rina Funk <rina.funk@stud.fh-rosenheim.de>
2019-04-16 23:02:30 +0200

    Neuen Button hinzugefügt

--- frontend/src/components/TheChat.vue
+++ frontend/src/components/TheChat.vue
@@ -1,54 +1,46 @@
 <template>
+    <div class="container">
+        <form class="chat-area">   
+            <div class="chat-area--messages">
+                <p id="text">
+                    Messages
+                </p>
+            </div>
 
-    <form class="chat-area">
-        
-        <div class="chat-area--messages">
-            <p id="text">
-                Test the chat window by sending a message:
-            </p>
-        
-        </div>
+            <v-textarea class="chat-area--text" placeholder="Write a message..."></v-textarea>
+            <v-btn color="info">Send</v-btn>   
 
-        <textarea placeholder="Write a test message...." class="chat-area--text" >
-        </textarea> 
-        
-        <button class="chat-area--button"> 
-                Send Message! 
-        </button>
-
-    </form>
-
+        </form>
+    </div>
 </template>
 
 <style lang="scss">
-
-#text{
-    margin-left: 50px;
-    margin-right: 50px;
-    margin-bottom: 250px;
-    padding: 0px;
-    text-align: center;
-    width: 500px;
-}
-
-.chat-area--text{
-    background: rgb(255, 255, 255); 
-    color: rgb(34, 34, 34);
-    margin-left: 5px;
-    margin-right: 5px;
-    margin-bottom: 100px;
-    padding: 0px;
-}
-.chat-area--button{
-    background: rgb(78, 140, 255); 
-    color: rgb(255, 255, 255);
-}
-.chat-area--messages{
-    padding-left: 5px;
-    padding-right: 5px;
-    padding-bottom: 5px;
-}
-
+    #text{
+        padding: 0px;
+        height: 250px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        
+    }
+    .container{
+        margin-right: auto; 
+        margin-left: auto;
+    }
+    .chat-area--text{
+        background: rgb(255, 255, 255); 
+        color: rgb(34, 34, 34);
+        margin-left: 5px;
+        margin-right: 5px;
+        margin-bottom: 100px;
+        padding: 0px;
+    }
+    .chat-area--button{ 
+        
+    }
+    .chat-area--messages{
+        padding: 0px;
+    }
 </style>
 
 
