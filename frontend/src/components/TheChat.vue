51f8496c6f8f25c6ce19775148a608f564652108
Rina Funk <rina.funk@stud.fh-rosenheim.de>
2019-05-03 20:59:04 +0200

    Update

--- frontend/src/components/TheChat.vue
+++ frontend/src/components/TheChat.vue
@@ -1,46 +1,120 @@
 <template>
     <div class="container">
-        <form class="chat-area">   
-            <div class="chat-area--messages">
-                <p id="text">
-                    Messages
-                </p>
+
+        <!--chat-history-->  
+      <div class="chat-history">
+        <ul>
+          <li class="clearfix">
+            <div class="message-data align-right">
+              <span class="message-data-time" >10:10 AM, Today</span> &nbsp; &nbsp;
+              <span class="message-data-name" >Olia</span> <i class="fa fa-circle me"></i>
+              
             </div>
+            <div class="message other-message float-right">
+              Hi Vincent, how are you? How is the project coming along?
+            </div>
+          </li>
+          
+          <li>
+            <div class="message-data">
+              <span class="message-data-name"><i class="fa fa-circle online"></i> Vincent</span>
+              <span class="message-data-time">10:12 AM, Today</span>
+            </div>
+            <div class="message my-message">
+              Are we meeting today? Project has been already finished and I have results to show you.
+            </div>
+          </li>
+        </ul>
+      </div>
+        <!--end of chat history-->
+        
+        
+        <div class="chat-message">
+            <textarea name="message-to-send" id="message-to-send" placeholder ="Type your message" rows="3"></textarea>
+            
+            <button>Send</button>
+        </div> 
 
-            <v-textarea class="chat-area--text" placeholder="Write a message..."></v-textarea>
-            <v-btn color="info">Send</v-btn>   
-
-        </form>
     </div>
 </template>
 
 <style lang="scss">
-    #text{
-        padding: 0px;
-        height: 250px;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        
-    }
+    $green: #86BB71;
+    $blue: #94C2ED;
+    $orange: #E38968;
+    $gray: #92959E;
     .container{
         margin-right: auto; 
         margin-left: auto;
     }
-    .chat-area--text{
-        background: rgb(255, 255, 255); 
-        color: rgb(34, 34, 34);
-        margin-left: 5px;
-        margin-right: 5px;
-        margin-bottom: 100px;
-        padding: 0px;
-    }
-    .chat-area--button{ 
+
+    .chat-history {
+    padding: 30px 30px 20px;
+    border-bottom: 2px solid white;
+    overflow-y: scroll;
+    height: 575px;
+    
+        .message-data {
+        margin-bottom: 15px;
+        }
+        
+        .message-data-time {
+        color: lighten($gray, 8%);
+        padding-left: 6px;
+        }
         
     }
-    .chat-area--messages{
-        padding: 0px;
+
+    .align-right {
+        text-align: right;
     }
+
+    .float-right {
+        float: right;
+    }
+
+    .chat-message {
+    padding: 30px;
+    
+        textarea {
+        width: 100%;
+        border: none;
+        padding: 10px 20px;
+        font: 14px/22px "Lato", Arial, sans-serif;
+        margin-bottom: 10px;
+        border-radius: 5px;
+        resize: none;
+
+        }
+    
+        button {
+        float: right;
+        color: #1E90FF;
+        font-size: 16px;
+        text-transform: uppercase;
+        border: none;
+        cursor: pointer;
+        font-weight: bold;
+        background: #F2F5F8;
+        }
+    }
+    .my-message {
+      background: $green;
+    }
+    
+    .other-message {
+      background: $blue;
+    }
+
+    .clearfix:after {
+        visibility: hidden;
+        display: block;
+        font-size: 0;
+        content: " ";
+        clear: both;
+        height: 0;
+    }
+
 </style>
 
 
@@ -51,4 +125,6 @@ import { Component, Vue } from 'vue-property-decorator';
 export default class Chat extends Vue {
     data(){}
 }
+
+
 </script>
