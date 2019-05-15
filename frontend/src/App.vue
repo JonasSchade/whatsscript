bd35a51b25e63614c5dc4684563ce8c312a38609
Rina Funk <rina.funk@stud.fh-rosenheim.de>
2019-05-15 14:53:42 +0200

    added Login to App.vue

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,7 @@
 <template>
     <v-app >
       <TheToolbar />
-      <TheChat />
+      <TheLogin />
       <router-view />
     </v-app>
 </template>
@@ -10,9 +10,10 @@
 import { Component, Vue } from 'vue-property-decorator';
 import TheToolbar from '@/components/TheToolbar.vue';
 import TheChat from '@/components/TheChat.vue';
+import TheLogin from '@/components/TheLogin.vue';
 
 @Component({
-    components: { TheToolbar, TheChat }
+    components: { TheToolbar, TheChat, TheLogin }
 })
 
 export default class App extends Vue {}
