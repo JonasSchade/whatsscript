cd4cf73699e1d4fda200382510af0b0f0dcf0267
Rina Funk <rina.funk@stud.fh-rosenheim.de>
2019-05-15 14:04:11 +0200

    Added Login component

---
+++ frontend/src/components/TheLogin.vue
@@ -0,0 +1,15 @@
+<template>
+    
+</template>
+
+<style lang="scss">
+
+</style>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Login extends Vue {}
+</script> 
+
