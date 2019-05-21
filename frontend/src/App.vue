e8fa4dce91a1139b815b4c1a549bb94ddfc5f066
Rina Funk <rina.funk@stud.fh-rosenheim.de>
2019-05-21 16:26:35 +0200

    Login aus router entfernt

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,6 @@
 <template>
     <v-app >
       <TheToolbar />
-      <TheLogin />
       <router-view />
     </v-app>
 </template>
@@ -11,6 +10,7 @@ import { Component, Vue } from 'vue-property-decorator';
 import TheToolbar from '@/components/TheToolbar.vue';
 import TheChat from '@/components/TheChat.vue';
 import TheLogin from '@/components/TheLogin.vue';
+import TheSettings from '@/components/TheSettings.vue';
 
 @Component({
     components: { TheToolbar, TheChat, TheLogin }
